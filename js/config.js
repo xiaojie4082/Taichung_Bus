285bec5b5f5ccde795ec0185121e33e65872fbcb
xiaojie4082 <xiaojie4082@gmail.com>
2025-05-12 20:32:27 +0800

    更新 config.js，將 Firebase 配置中的環境變數替換為實際的 API 金鑰和專案設定，以便於開發和測試。

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    apiKey: "AIzaSyDVSASjewTZJexL64aFf3nQL2gA5bzO5b4",
+    authDomain: "taichungbus-a6cc5.firebaseapp.com",
+    projectId: "taichungbus-a6cc5",
+    storageBucket: "taichungbus-a6cc5.firebasestorage.app",
+    messagingSenderId: "959390803671",
+    appId: "1:959390803671:web:bc5c79576e5884737cf292",
+    measurementId: "G-2W3F922KRZ",
+    databaseURL: "https://taichungbus-a6cc5-default-rtdb.firebaseio.com"
 };
 
 export { firebaseConfig }; 
